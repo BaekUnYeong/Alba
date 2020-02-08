7dc4b8c08488f6c510c0151db1d7edaee19c1b76
백운영 <dns6615@gmail.com>
2020-02-08 17:29:58 +0900

    commit

--- src/main/java/kr/or/ddit/alba/dao/AlbaDAOImpl.java
+++ src/main/java/kr/or/ddit/alba/dao/AlbaDAOImpl.java
@@ -2,40 +2,65 @@ package kr.or.ddit.alba.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import kr.or.ddit.mybatis.CustomSqlSessioFactoryBuilder;
 import kr.or.ddit.vo.AlbaVO;
 import kr.or.ddit.vo.PagingVO;
 
 public class AlbaDAOImpl implements IAlbaDAO {
-
+	SqlSessionFactory sqlSessionFactory =
+			CustomSqlSessioFactoryBuilder.getSqlSessionFactory();
+	
 	@Override
 	public int selectAlbaCount(PagingVO<AlbaVO> pagingVO) {
-		// TODO Auto-generated method stub
-		return 0;
+		try(
+			SqlSession sqlSession = sqlSessionFactory.openSession();
+		) {
+			IAlbaDAO mapper = sqlSession.getMapper(IAlbaDAO.class);
+			return mapper.selectAlbaCount(pagingVO);
+		}
 	}
 
 	@Override
 	public List<AlbaVO> selectAlbaList(PagingVO<AlbaVO> pagingVO) {
-		// TODO Auto-generated method stub
-		return null;
+		try(
+			SqlSession sqlSession = sqlSessionFactory.openSession();
+		) {
+			IAlbaDAO mapper = sqlSession.getMapper(IAlbaDAO.class);
+			return mapper.selectAlbaList(pagingVO);
+		}
 	}
 
 	@Override
-	public AlbaVO selectAlba(String buyer_id) {
-		// TODO Auto-generated method stub
-		return null;
+	public AlbaVO selectAlba(String al_id) {
+		try(
+			SqlSession sqlSession = sqlSessionFactory.openSession();
+		) {
+			IAlbaDAO mapper = sqlSession.getMapper(IAlbaDAO.class);
+			return mapper.selectAlba(al_id);
+		}
 	}
 
 	@Override
-	public int insertAlba(AlbaVO buyer) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int insertAlba(AlbaVO alba) {//insert 수정 필요 TODO
+		try(
+			SqlSession sqlSession = sqlSessionFactory.openSession();
+		) {
+			IAlbaDAO mapper = sqlSession.getMapper(IAlbaDAO.class);
+			return mapper.insertAlba(alba);
+		}
 	}
 
 	@Override
-	public int updateAlba(AlbaVO buyer) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int updateAlba(AlbaVO alba) {//update 수정 필요 TODO
+		try(
+			SqlSession sqlSession = sqlSessionFactory.openSession();
+		) {
+			IAlbaDAO mapper = sqlSession.getMapper(IAlbaDAO.class);
+			return mapper.updateAlba(alba);
+		}
 	}
-
 
 }
