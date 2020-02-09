81f2004bac971b1202a413d81d3f535e05a8e860
HanSeunghee <shin@172.16.11.202>
2020-02-10 02:21:50 +0900

    라이센스 이미지 첨부 컨트롤러 작성 및 주석 추가. 주석에는 service, dao, mapper를 설계함.

--- src/main/java/kr/or/ddit/alba/controller/AlbaLicenseImageController.java
+++ src/main/java/kr/or/ddit/alba/controller/AlbaLicenseImageController.java
@@ -5,23 +5,48 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.or.ddit.alba.service.AlbaServiceImpl;
+import kr.or.ddit.alba.service.IAlbaService;
 import kr.or.ddit.file.FileUploadRequestWrapper;
 import kr.or.ddit.file.PartWrapper;
 import kr.or.ddit.mvc.annotation.CommandHandler;
+import kr.or.ddit.mvc.annotation.HttpMethod;
 import kr.or.ddit.mvc.annotation.URIMapping;
 import kr.or.ddit.vo.LicenseVO;
 
 @CommandHandler
+/**
+ * req.setAttribute("licenseVO", licenseVO);
+ * req.getRequestDispatcher(req.getContentType()+"/alba/albaLicense.do")
+ * 		.include(req, resp);
+ * req.getAttribute("licenseVO");
+ * 위와 같은 순서로 진행할 것.
+ * licenseVO를 setAttribute하여 넣어준다. 혹은 넣지 않을 경우 자동 생성됨.
+ * 이 컨트롤러의 Dispatcher를 호출하여 include한다.
+ * req에 파일이 들어있다면 (FileUploadRequestWrapper를 instanceof로 만족할 때)
+ * 이 컨트롤러가 licenseVO에 setter를 호출하여 넣어준다.
+ * 그 후 req.setAttribute("licenseVO", license);를 통해 Attribute한다.
+ * 사용자는 req.getAttribute("licenseVO");를 호출하면 VO를 반환받을 수 있다.
+ * 
+ * 이 컨트롤러를 사용하는 곳은(Insert, Update) Dao에서 Alba뿐만이 아닌
+ * license와 lic_alba 테이블을 고려해야 한다. 테이블 이름 체크할것. TODO
+ */
 public class AlbaLicenseImageController {
-	@URIMapping(value = "/alba/albaLicense.do")
+	IAlbaService service = new AlbaServiceImpl();
+	
+	@URIMapping(value = "/alba/albaLicense.do", method = HttpMethod.POST)
 	public String imageUpload(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		LicenseVO license = new LicenseVO();
-		//setAttribute할것. 참조형
+		LicenseVO license = (LicenseVO) req.getAttribute("licenseVO");
+		if(license == null) {
+			license = new LicenseVO();
+			//service 호출 TODO lic_code를 set해야 한다.
+		}
+		req.setAttribute("licenseVO", license);
 		
 		if(req instanceof FileUploadRequestWrapper) {
 			PartWrapper imageFile = 
 					((FileUploadRequestWrapper) req).
-					getPartWrapper("??");//chrome F12 활용할것.
+					getPartWrapper("??");//chrome F12 활용할것. TODO
 			byte[] lic_img = imageFile.getBytes();
 			license.setLic_img(lic_img);
 		}
@@ -31,59 +56,30 @@ public class AlbaLicenseImageController {
 }
 
 /*
-@URIMapping(value="/member/memberInsert.do")
-	public String form(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String goPage = "member/memberForm";
-		return goPage;
+	service에 lic_code를 생성하는 코드를 만들것.
+	
+	반환값이 String인가? 체크할것. TODO
+	public String getNew_Lic_code();
+	public String getNew_Lic_code(){
+		return dao.getNew_Lic_code();
 	}
-
-	@URIMapping(value="/member/memberInsert.do", method = HttpMethod.POST)
-	public String insert(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		1. 요청 분석 (파라미터 검증 포함)
-		MemberVO member = new MemberVO(); // command object
-		req.setAttribute("member", member);
-		try {
-			BeanUtils.populate(member, req.getParameterMap());
-		} catch (IllegalAccessException | InvocationTargetException e) {
-			throw new RuntimeException(e);
+	
+	dao에 lic_code를 생성하는 코드를 만들것.
+	
+	public String getNew_Lic_code();
+	public String getNew_Lic_code(){
+		try(
+			SqlSession sqlSession = sqlSessionFactory.openSession();
+		){
+			IAlbaDAO mapper = sqlSession.getMapper(IAlbaDAO.class);
+			return mapper.getNew_Lic_code();
 		}
-		
-		if(req instanceof FileUploadRequestWrapper) {
-			PartWrapper imageFile = ((FileUploadRequestWrapper) req).getPartWrapper("mem_image");
-			byte[] mem_img = imageFile.getBytes();
-			member.setMem_img(mem_img);
-		}
-		
-		Map<String, List<CharSequence>> errors = new HashMap<>();
-		req.setAttribute("errors", errors);
-		GeneralValidator validator = new GeneralValidator();
-		boolean valid = validator.validate(member, errors, InsertHint.class);
-		String goPage = null;
-		String message = null;
-		if (valid) {
-//		2. 로직 선택 service.createMember(member)
-			ServiceResult result = service.createMember(member);
-//		3. 로직의 실행 결과에 따른 분기 페이지 선택
-			switch (result) {
-			case OK:
-				goPage = "redirect:/";
-				break;
-			case PKDUPLICATED:
-				message = "아이디 중복";
-				goPage = "member/memberForm";
-				break;
-
-			default: // FAIL
-				message = "서버 오류, 잠시뒤 다시 .";
-				goPage = "member/memberForm";
-				break;
-			}
-		} else {
-			goPage = "member/memberForm";
-		}
-		req.setAttribute("message", message);
-//		4. 모델 데이터 공유
-//		5. 해당 뷰로 이동
-		return goPage;
 	}
+	
+	xml에 max(lic_code)+1을 반환하는 코드 생성
+	
+	<select id="getNew_Lic_code" resultType="String">
+		SELECT MAX(lic_code)+1
+		FROM license;	//TODO ?? sql 확인해볼것.
+	</select>
 */
